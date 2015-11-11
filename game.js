3dd8b2d5668f99ab0047b8348de92b30ca66f9fc
Mary Goodwin <mary.goodwin@milliman.com>
2015-11-11 14:43:47 -0800

    added images to responses

--- game.js
+++ game.js
@@ -15,7 +15,8 @@ var questionOne = prompt(questions[0]);
 questionOne = questionOne.toLowerCase();
 if(questionOne === answers[0] || questionOne === answers[1]) {
 	console.log("Guess 1 is: " + questionOne);
-	document.getElementById('q1').innerHTML = "You guessed " + questionOne + "! You were right :) My favorite color is red!";
+	document.getElementById('q1').innerHTML = "You guessed " + questionOne + "! You were right :) My favorite color is red (much like Tedd's cowboy boots)!";
+	document.getElementById('image1').innerHTML = '<div><img src="http://www.hercampus.com/sites/default/files/styles/full_width_embed/public/2014/03/27/Red%20cowboy%20boots.jpg?itok=67dXqRY3" alt="Red cowboy boots" width="200" height="135" /></div>';
 } else {
 	console.log("Guess 1 is: " + questionOne);
 	document.getElementById('q1').innerHTML = "You guessed " + questionOne + "! You were wrong :(";
@@ -25,7 +26,8 @@ var questionTwo = prompt(questions[1]);
 questionTwo = questionTwo.toLowerCase();
 if(questionTwo === answers[0] || questionTwo === answers[1]) {
 	console.log("Guess 2 is: " + questionTwo);
-  	document.getElementById('q2').innerHTML = "You guessed " + questionTwo + "! You were right :) I am from Seattle!";
+  	document.getElementById('q2').innerHTML = "You guessed " + questionTwo + "! You were right :) I am from Seattle! Here's a pretty picture of it!";
+	document.getElementById('image2').innerHTML = '<div><img src="http://www.socialventurepartners.org/seattle/wp-content/uploads/sites/40/2013/06/shutterstock_137180738.jpg" alt="Seattle Skyline" width="200" height="80" /></div>';
 } else {
 	console.log("Guess 2 is: " + questionTwo);
 	document.getElementById('q2').innerHTML = "You guessed " + questionTwo + ". You were wrong :(";
@@ -35,7 +37,8 @@ var questionThree = prompt(questions[2]);
 questionThree = questionThree.toLowerCase();
 if(questionThree === answers[2] || questionThree === answers[3]) {
 	console.log("Guess 3 is: " + questionThree);
-  	document.getElementById('q3').innerHTML = "You guessed " + questionThree + "! You were right :) I am actually 26!";
+  	document.getElementById('q3').innerHTML = "You guessed " + questionThree + "! You were right :) I am actually 26! This was my Halloween costume this year!";
+	document.getElementById('image3').innerHTML = '<div><img src="https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xtp1/v/t1.0-9/11260783_10205329939559758_5326235292908423515_n.jpg?oh=4f7c559266a5277cb08e296771ad0e71&oe=56BFB30E" alt="Mary" width="200" height="200" /></div>';
 } else {
 	console.log("Guess 3 is: " + questionThree);
 	document.getElementById('q3').innerHTML = "You guessed " + questionThree + ". You were wrong :( I am actually 26.";
---
+++ readme.md
@@ -0,0 +1,7 @@
+Programmer: Mary Goodwin
+Project Name: Guesing Game
+
+Description: This project is a JavaScript game developed to help users get to know me using a series of Yes/No questions. These questions range from my favorite color, to my age, to the location where I live.
+
+Status: Changed questions from basic prompts to arrays.
+Status 2: Added images to correct responses.
